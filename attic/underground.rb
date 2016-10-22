6eda8d1e7572fe280cd09bbfe5c07f77320cd154
s. rannou <mxs@sbrk.org>
2016-10-22 22:44:21 +0100

    Added acid melody.

--- attic/underground.rb
+++ attic/underground.rb
@@ -1,5 +1,3 @@
-# Still still WIP
-
 use_bpm 124
 
 define :mplay do |c|
@@ -10,14 +8,10 @@ define :mplay do |c|
     sample :bd_klub
   end
   if c == 's'
-    with_fx :reverb, mix: 0.5 do
-      with_fx :krush do
-        sample :bd_fat, start: 0.15, finish: 0.2, rate: 0.4, amp: 0.25
-      end
-    end
+    sample :bd_fat, start: 0.15, finish: 0.2, rate: 0.4, amp: 0.25
   end
   if c == 'c'
-    synth :noise, release: 0.125, amp: 0.25, res: 0.999, cutoff: :E3
+    synth :noise, release: 0.1, amp: 2, res: 0.75, cutoff: :E4
   end
 end
 
@@ -29,8 +23,6 @@ live_loop :main do
   patterns = [
     clear("k... .... k... ...."),
     clear(".... .... s... ...."),
-    clear(".... .... .... ...."),
-    clear(".c.. ..c. ..c. c..."),
   ].ring
   
   16.times do |i|
@@ -47,7 +39,7 @@ cutoff = [-5, 0, 5, 12]
 variations = "_-__-`____-``_``".ring
 
 live_loop :dark_lead, sync: :main do
-  c = 0
+  c = 24
   with_fx :wobble, phase: 1, wave: 1, cutoff_min: :E2 + c, cutoff_max: :E6 do
     with_fx :bitcrusher do
       s = synth :fm, pulse_width: 0.9, sustain: notes.length, release: 0, amp: 0.25
@@ -61,21 +53,30 @@ live_loop :dark_lead, sync: :main do
 end
 
 live_loop :melody, sync: :main do
-  total = 0
-  with_fx :vowel, voice: 3 do
-    with_fx :flanger do
-      [[:E3, 0.25], [:C4, 0.25], [:E3, 0.75], [:Gs3, 0.5], [:G3, 0.75], [:G3, 0.25], [:E3, 1]].each do |n, r|
-        synth :fm, note: n, release: r, amp: 2
-        sleep r
-        total = total + r
+  with_fx :lpf, cutoff: 100 do
+    with_fx :gverb, mix: 0.25 do
+      with_fx :flanger do
+        [
+          [[:E3, 0.5], [:E3, 0.25], [:E3, 0.75], [:E3, 1], [:E3, 0.5], [:G3, 0.25], [:E3, 1]],
+          [[:E3, 0.5], [:E3, 0.25], [:E3, 0.75], [:E3, 1], [:E3, 0.5], [:G3, 0.25], [:E3, 1]],
+          [[:E3, 0.5], [:E3, 0.25], [:E3, 0.75], [:E3, 1], [:E3, 0.5], [:G3, 0.25], [:E3, 1]],
+          [[:E3, 0.5], [:E3, 0.25], [:G3, 0.75], [:E3, 1], [:Gs3, 0.5], [:G3, 0.25], [:G3, 1]],
+        ].each do |mel|
+          total = 0
+          mel.each do |n, r|
+            synth :pluck, note: n + 12, res: 0, release: r
+            sleep r
+            total = total + r
+          end
+          sleep 4 - total
+        end
       end
     end
   end
-  sleep 4 - total
 end
 
 live_loop :dark_bass, sync: :main do
-  c = 0
+  c = 24
   with_fx :wobble, phase: 16, invert_wave: 1, phase: 32, cutoff_min: 100, cutoff_max: 110 do
     with_fx :wobble, phase: 1, res: 0, smooth_down: 0.2, smooth_down: 0.2 do
       notes.each do |n|
@@ -92,3 +93,4 @@ live_loop :dark_bass, sync: :main do
     end
   end
 end
+
